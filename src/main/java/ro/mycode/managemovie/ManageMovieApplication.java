72de686345dff2c9011ad213e6b37f6b31416038
FloreDenis2001 <floredenis907@yahoo.com>
2023-01-13 23:47:48 +0200

    master

---
+++ src/main/java/ro/mycode/managemovie/ManageMovieApplication.java
@@ -0,0 +1,13 @@
+package ro.mycode.managemovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mycode/managemovie/ManageMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mycode.managemovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
